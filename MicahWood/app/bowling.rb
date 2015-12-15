6daab658e1342acd90846400d9e0837933e24f6c
Micah Wood <micah.wood@gmail.com>
2015-12-14 23:31:03 -0700

    First draft

---
+++ MicahWood/app/bowling.rb
@@ -0,0 +1,50 @@
+class Bowling
+
+  def initialize
+    @rolls = []
+  end
+
+  def roll(pins)
+    if you_were_over_the_line(pins)
+      print "MARK IT ZERO!"
+      pins = 0
+    end
+    @rolls << pins
+  end
+
+  def score(rolls = nil)
+    if rolls
+      rolls.each { |r| roll(r) }
+    end
+    score = 0
+    roll = 0
+
+    (1..10).each do |frame|
+      if is_strike(roll)
+        score += 10 + @rolls[roll + 1] + @rolls[roll + 2]
+        roll += 1
+      elsif is_spare(roll)
+        score += 10 + @rolls[roll + 2]
+        roll += 2
+      else
+        score += @rolls[roll] + @rolls[roll + 1]
+        roll += 2
+      end
+    end
+    score
+  end
+
+  private
+    def is_spare(roll)
+      @rolls[roll] + @rolls[roll + 1] == 10
+    end
+
+    def is_strike(roll)
+      @rolls[roll] == 10
+    end
+
+    def you_were_over_the_line(pins)
+      (! pins.is_a? Integer) || pins > 10 || pins < 0
+    end
+
+end
---
+++ MicahWood/tests/test_bowling.rb
@@ -0,0 +1,60 @@
+require 'minitest/autorun'
+require 'app/bowling'
+
+describe Bowling do
+  before do
+    @game = Bowling.new
+  end
+
+  it "can score a game of all gutter balls" do
+    (1..20).each { @game.roll(0) }
+    @game.score.must_equal 0
+  end
+
+  it "can score the sum of all knocked down pins" do
+    (1..20).each { @game.roll(1) }
+    @game.score.must_equal 20
+  end
+
+  it "awards a one roll bonus for every spare" do
+    @game.roll(2)
+    @game.roll(8) # we got a spare
+    @game.roll(5)
+
+    (1..17).each { @game.roll(0) }
+    @game.score.must_equal 20
+  end
+
+  it "awards a two roll bonus for every strike" do
+    @game.roll(10)
+    @game.roll(7)
+    @game.roll(2)
+
+    (1..17).each { @game.roll(0) }
+    @game.score.must_equal 28
+  end
+
+  it "scores a perfect game" do
+    (1..12).each { @game.roll(10) }
+    @game.score.must_equal 300
+  end
+
+  it "follows Walter Sobchak rules for cheating" do
+    proc { @game.roll(-10) }.must_output "MARK IT ZERO!"
+    proc { @game.roll(20) }.must_output "MARK IT ZERO!"
+    proc { @game.roll('A') }.must_output "MARK IT ZERO!"
+    proc { @game.roll([]) }.must_output "MARK IT ZERO!"
+    proc { @game.roll(Object.new) }.must_output "MARK IT ZERO!"
+    (1..15).each { @game.roll(0) }
+    @game.score.must_equal 0
+  end
+
+  it "can score a game when given an array of rolls" do
+    @game.score(Array.new(12, 10)).must_equal 300
+  end
+
+  it "can score a game full of people who are about to enter a world of pain" do
+    rolls = [-4, 'a', [], nil, 32, Object.new, Hash.new, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    @game.score(rolls).must_equal 0
+  end
+end
